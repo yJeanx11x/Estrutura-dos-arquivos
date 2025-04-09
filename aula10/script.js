03df660196647cd88848840bf40a3dc3db42bf1b
Jean Lucas <j3anlucas010@gmail.com>
2025-04-09 10:59:07 -0300

    Métado reduce()

--- aula10/script.js
+++ aula10/script.js
@@ -1,6 +1,13 @@
 // Método some()
 const ages = [15, 30, 39, 29];
-const result=ages.some((age)=>age <18)
+const result = ages.some((age) => age < 18);
 
-console.log(result)
+console.log(result);
 
+// Métado reduce()
+
+const values = [1, 2, 3, 4, 5];
+const sum=values.reduce((accumulator,currentvalue)=>{
+return accumulator + currentvalue
+},0)
+console.log(sum)
