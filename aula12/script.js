8ade24ccc4c18c3e87b40e013fd09823c2f05935
Jean Lucas <j3anlucas010@gmail.com>
2025-04-14 17:25:11 -0300

    shallow freezing

--- aula12/script.js
+++ aula12/script.js
@@ -1,16 +1,41 @@
 // Imutabilidade
 
-const address1={
-  street:'Av.Brasil',
-  number:20,
-}
+const address1 = {
+  street: "Av.Brasil",
+  number: 20,
+};
 // Isso não é uma copia. É uma referência.
-const address3=address1
-address3.number = 30
-console.log('##### referência')
-console.log(address3)
-console.log('###########')
+const address3 = address1;
+address3.number = 30;
+console.log("##### referência");
+console.log(address3);
+console.log("###########");
 // Cria um novo objeto utilizando as propriedades e valores de address1
-console.log('cria uma novo objeto utilizando as propriedades')
-const address2={...address1,street:'Rua Das Perobas',number:298}
-console.log(address1,address2)
+console.log("cria uma novo objeto utilizando as propriedades");
+const address2 = { ...address1, street: "Rua Das Perobas", number: 298 };
+console.log(address1, address2);
+
+
+// shallow copy (cópia superdicial)
+
+const htmlCourse={
+  course:'HTML',
+  students:[{name:'jean',email:'jean@hotmail.com'}]
+}
+
+const jsCourse={
+  ...htmlCourse,
+  course:'JavaScript'
+}
+console.log(htmlCourse,jsCourse)
+console.log('####### cópia Profunda')
+//Deep copy
+
+
+const jsCourse1={
+  ...htmlCourse,
+  course:'JavaScript',
+  students:[...htmlCourse.students,{name:'Souza',email:'souza@gmail.com'}]
+}
+
+console.log(jsCourse1)
