cd8d1c7facee55dc0b2aadfa3f47f0762a4a04c5
Jean Lucas <j3anlucas010@gmail.com>
2025-04-19 10:09:03 -0300

    Manipulando objetos imutáveis

--- aula13/script.js
+++ aula13/script.js
@@ -1,24 +1,24 @@
-// Shallow freezing 
-const book = {
-    title: "Objetos Imutáveis",
-    category: "javaScript",
+const book={
+  title:'Objetos Imutáveis',
+  category:'JavaScript',
+  author:{
+    name:'Jean Lucas',
+    email:'jean@gmail.com',
+    
+  },
+}
 
-    author:{
-      name:'Jean',
-      email:'jean@gmail.com'
-    }
-  }
-// O JavaScript em si não impõe restiçãoes á modificação dos objetos.
-
-// book.category='css'
-// console.log(book)
-
-// congela o objeto e impede a modificação
-
-Object.freeze(book)
-book.category='css' // não modifica 
-
-
-// o object.freeze() não impede modificaçãoes profundas em objetos aninhados (shallow freezing).
-book.author.name='lucas'
+const updtedBook={
+  ...book,
+  title:'Aprendendo Css',
+  category:'CSS',
+}
+ // Original intacto.
 console.log(book)
+
+// Modificado.
+console.log(updtedBook)
+
+// Utilizando operador de dessetruturação (rest operator) para remover propriedades
+const {category,...RemovendoCategory}=book
+console.log(RemovendoCategory)
